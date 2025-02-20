31668ee05ea2c1d43ec6913fe412e6044154ac70
Maya <maya.kolomoetc@yandex.ru>
2025-02-20 19:40:36 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,25 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base:'/romanticc/',
-	publicDir: 'public',
-	build: {
-		rollupOptions:{
-			input:{
-				main: 'index.html',
-				second: 'second_page.html',
-				third: 'third_page.html',
-				four: 'fourth_page.html',
-				five: 'fives_page.html',
-				six: 'sixth_page.html',
-				seven: 'seven_page.html',
-				last: 'last.html',
-				eight: 'eight_page.html',
-				nine: 'nine_page.html',
-			}
-		}
-	}
-})
+  base: '/romanticc/',
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        second: 'second_page.html',
+        third: 'third_page.html',
+        four: 'fourth_page.html',
+        five: 'fives_page.html',
+        six: 'sixth_page.html',
+        seven: 'seven_page.html',
+        last: 'last.html',
+        eight: 'eight_page.html',
+        nine: 'nine_page.html',
+      }
+    }
+  },
+  css: {
+    // Настройки CSS, если они нужны
+  }
+});
