82a1fcc67107de41dc0511a48efa173bf633ddc4
Maya <maya.kolomoetc@yandex.ru>
2025-02-20 19:28:20 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,16 @@ export default defineConfig({
 	publicDir: 'public',
 	build: {
 		rollupOptions:{
-			main: './index.html',
-			second: './second_page.html',
-			third: './third_page.html',
-			four: './fourth_page.html',
-			five: './fives_page.html',
-			six: './sixth_page.html',
-			seven: './seven_page.html',
-			last: './last.html',
-			eight: './eight_page.html',
-			nine: './nine_page.html',
+			main: 'index.html',
+			second: 'second_page.html',
+			third: 'third_page.html',
+			four: 'fourth_page.html',
+			five: 'fives_page.html',
+			six: 'sixth_page.html',
+			seven: 'seven_page.html',
+			last: 'last.html',
+			eight: 'eight_page.html',
+			nine: 'nine_page.html',
 		}
 	}
 })
